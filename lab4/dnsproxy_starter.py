72f70fef44302968539abd323377acf5f2f8ea54
hoseok-lee <47509132+hoseok-lee@users.noreply.github.com>
2022-04-08 22:47:01 -0400

    Debugging

--- lab4/dnsproxy_starter.py
+++ lab4/dnsproxy_starter.py
@@ -19,6 +19,7 @@ dns_port = args.dns_port
 SPOOF = args.spoof_response
 
 PROXY_IP = "127.0.0.1"
+socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((PROXY_IP, port))
 
